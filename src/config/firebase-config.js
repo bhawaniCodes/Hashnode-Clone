29a312380b95b4e3a350358d439239e8b17ceb55
Bhawani Shankar <bhaweshlohar2@gmail.com>
2021-11-02 15:03:37 +0530

    For firebase configuration Config file has added & firebase authenication initiated

---
+++ src/config/firebase-config.js
@@ -0,0 +1,27 @@
+// import { initializeApp } from "firebase/app";
+// import { getAuth } from "firebase/auth";
+// import firebase from "firebase/compat/app";
+// require("firebase/compat/auth");
+// require("firebase/compat/firestore");
+
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
+import "firebase/compat/firestore";
+
+
+const firebaseConfig = {
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_APP_ID,
+    measurementId: process.env.REACT_APP_MEASUREMENT_ID,
+};
+
+const app = firebase.initializeApp(firebaseConfig);
+export const auth = firebase.auth();
+export const googleProvider = new firebase.auth.GoogleAuthProvider();
+export const facebookProvider = new firebase.auth.FacebookAuthProvider();
+export const gitHubProvider = new firebase.auth.GithubAuthProvider();
+export default firebase;
